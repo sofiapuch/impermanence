e0d52ffe9cc172838f97c22625e2b12bbc574ffe
sophia <sophiepuch@gmail.com>
2014-05-22 20:28:42 +0200

    primera verso web

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 	
+	document.oncontextmenu = function(){return false}
+	
+	function removeHash () { 
+    	history.pushState("", document.title, window.location.pathname+ window.location.search);
+	};
+	
 	//-------------------my functions-----------------------
 	
 	function highlightSelected(e) {
 		$('.highlighted').removeClass("highlighted");
 		e.addClass("highlighted");
 	};
+	
+
 
 	//-------------------menu interaction-------------------
  
@@ -20,26 +28,31 @@ $(document).ready(function(){
 	//-------------------loading content-------------------
 	
 	$(".menu").find("#home").click( function() {
+		removeHash();
 		$('#content').load("home.html");
 		highlightSelected($(".menu").find("#home"));
 	});
 	
 	$(".menu").find("#photo").click( function() {
+		removeHash();
 		$('#content').load("photography.html");
 		highlightSelected($(".menu").find("#photo"));
 	});
 
 	$(".menu").find("#design").click( function() {
+		removeHash();
 		$('#content').load("design.html");
 		highlightSelected($(".menu").find("#design"));	
 	});
 	
 	$(".menu").find("#prog").click( function() {
+		removeHash();
 		$('#content').load("prog.html");	
 		highlightSelected($(".menu").find("#prog"));
 	});
 	
 	$(".menu").find("#contact").click( function() {
+		removeHash();
 		$('#content').load("contact.html");	
 		highlightSelected($(".menu").find("#contact"));
 	});
